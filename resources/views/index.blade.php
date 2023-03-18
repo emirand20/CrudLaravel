767ad08b82dcb8a1b96ecb2a0b3d9bf0d66be748
4Tipy <lchavez21@jaumebalmes.net>
2023-03-18 13:50:05 +0100

    controlador e index

---
+++ resources/views/index.blade.php
@@ -0,0 +1,26 @@
+<table>
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Velocidad</th>
+            <th>Posición</th>
+            <th>Físico</th>
+            <th>Ritmo</th>
+            <th>Regate</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($pol as $p)
+        <tr>
+            <td>{{ $p->id }}</td>
+            <td>{{ $p->nombre }}</td>
+            <td>{{ $p->velocidad }}</td>
+            <td>{{ $p->posicion }}</td>
+            <td>{{ $p->fisico }}</td>
+            <td>{{ $p->ritmo }}</td>
+            <td>{{ $p->regate }}</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
